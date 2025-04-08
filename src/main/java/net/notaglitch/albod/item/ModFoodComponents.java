938d49c9eb847c81505b3db15a33b0fabcceb346
Andromeda <andromeda@notaglitch.net>
2025-04-08 15:18:13 -0400

    Removed regen effect from blueberries

--- src/main/java/net/notaglitch/albod/item/ModFoodComponents.java
+++ src/main/java/net/notaglitch/albod/item/ModFoodComponents.java
@@ -5,8 +5,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoodComponents {
-    public static final FoodComponent BLUEBERRY = new FoodComponent.Builder().snack().nutrition(2).saturationModifier(0.15f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 40), 0.10f).build();
+    public static final FoodComponent BLUEBERRY = new FoodComponent.Builder().snack().nutrition(2).saturationModifier(0.15f).build();
 
 
     public static final FoodComponent BLUEBERRY_CHEESECAKE_SLICE = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
