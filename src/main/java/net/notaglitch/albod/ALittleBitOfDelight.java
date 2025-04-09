4b865ef1b42cbc897f703aad19d082db7ba76b92
Andromeda <andromeda@notaglitch.net>
2025-04-09 16:34:19 -0400

    Added working seasons

--- src/main/java/net/notaglitch/albod/ALittleBitOfDelight.java
+++ src/main/java/net/notaglitch/albod/ALittleBitOfDelight.java
@@ -2,6 +2,7 @@ package net.notaglitch.albod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.registry.tag.TagKey;
 import net.notaglitch.albod.block.AModBlocks;
@@ -17,9 +18,10 @@ public class ALittleBitOfDelight implements ModInitializer {
 	public static final String MOD_ID = "albod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final TagKey<Item> KNIVES = ModTags.KNIVES;
-
+	public static boolean SEASON_INSTALL;
 	@Override
 	public void onInitialize() {
+		SEASON_INSTALL = FabricLoader.getInstance().isModLoaded("seasons");
 		ModItemGroups.registerItemGroups();
 		AModItems.registerModItems();
 		ModSounds.registerSounds();
---
+++ src/main/java/net/notaglitch/albod/mixin/BlueberryBushBlockMixin.java
@@ -0,0 +1,41 @@
+package net.notaglitch.albod.mixin;
+
+import io.github.lucaargolo.seasons.FabricSeasons;
+import io.github.lucaargolo.seasons.utils.Season;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.notaglitch.albod.ALittleBitOfDelight;
+import net.notaglitch.albod.block.custom.BlueberryBushBlock;
+
+public class BlueberryBushBlockMixin extends BlueberryBushBlock {
+
+    public BlueberryBushBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!ALittleBitOfDelight.SEASON_INSTALL){
+super.randomTick(state, world, pos, random);
+            return;
+        }
+
+        Season season = FabricSeasons.getCurrentSeason();
+        if (season != Season.SUMMER && season != Season.FALL) return;
+        int age = this.getAge(state);
+        if (age < this.getMaxAge() - 1) super.randomTick(state, world, pos, random);
+        else if (season == Season.FALL) super.randomTick(state, world, pos, random);
+    }
+
+    private int getMaxAge() {
+        return (BlueberryBushBlock.MAX_AGE);
+    }
+
+    private int getAge(BlockState state) {
+        return (state.get(BlueberryBushBlock.AGE));
+    }
+
+}
